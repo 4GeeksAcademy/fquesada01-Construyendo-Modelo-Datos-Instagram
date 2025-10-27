03cfc4f7124eaeb1e1e39f261479586f1fbd5f34
fernandoqm01 <fernandoquesadamora@gmail.com>
2025-10-27 22:44:56 +0000

    modals

--- src/models.py
+++ src/models.py
@@ -1,19 +1,99 @@
-from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import String, Boolean
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import Column, ForeignKey, Boolean, Integer, String, DateTime, Text
+from sqlalchemy.orm import relationship, declarative_base
+from sqlalchemy import create_engine
+from eralchemy2 import render_er
 
-db = SQLAlchemy()
-
-class User(db.Model):
-    id: Mapped[int] = mapped_column(primary_key=True)
-    email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
-    password: Mapped[str] = mapped_column(nullable=False)
-    is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
+Base = declarative_base()
 
 
-    def serialize(self):
-        return {
-            "id": self.id,
-            "email": self.email,
-            # do not serialize the password, its a security breach
-        }
+class User(Base):
+    __tablename__ = 'user'
+
+    id = Column(Integer, primary_key=True)
+    username = Column(String(60), unique=True, nullable=False)
+    email = Column(String(120), unique=True, nullable=False)
+    password = Column(nullable=False)
+    profile_picture = Column(String(300))
+    bio = Column(Text)
+    is_active = Column(Boolean(), nullable=False)
+
+    post = relationship("Post", back_populates="user")
+    comments = relationship("Comment", back_populates="user")
+    likes = relationship("Like", back_populates="user")
+    media = relationship("Media", back_populates="user")
+
+    followers = relationship(
+        "Follower",
+        foreign_keys="Follower.followed_id",
+        back_populates="followed"
+    )
+    following = relationship(
+        "Follower",
+        foreign_keys="Follower.follower_id",
+        back_populates="follower"
+    )
+
+
+class Post(Base):
+    __tablename__ = 'post'
+    id = Column(Integer, primary_key=True)
+    image_url = Column(String(300), nullable=False)
+    caption = Column(Text)
+    created_at = Column(DateTime)
+    user_id = Column(Integer, ForeignKey('user.id'))
+
+    user = relationship("User", back_populates="post")
+    comments = relationship("Comment", back_populates="post")
+    likes = relationship("Like", back_populates="post")
+    media = relationship("Media", back_populates="post")
+
+
+class Media(Base):
+    __tablename__ = 'media'
+    id = Column(Integer, primary_key=True)
+    type = Column(String(200), nullable=True)
+    url = Column(String(300),)
+    post_id = Column(Integer, ForeignKey('post.id'))
+
+    post = relationship("Post", back_populates="media")
+
+
+class Comment(Base):
+    __tablename__ = 'comment'
+    id = Column(Integer, primary_key=True)
+    content = Column(Text, nullable=False)
+    created_at = Column(DateTime)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    post_id = Column(Integer, ForeignKey('post.id'))
+
+    user = relationship("User", back_populates="comments")
+    post = relationship("Post", back_populates="comments")
+
+
+class like(Base):
+    __tablename__ = 'like'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    post_id = Column(Integer, ForeignKey('post.id'))
+
+    user = relationship("User", back_populates="likes")
+    post = relationship("Post", back_populates="likes")
+
+
+class Follower(Base):
+    __tablename__ = 'follower'
+    id = Column(Integer, primary_key=True)
+    follower_id = Column(Integer, ForeignKey('user.id'))
+    followed_id = Column(Integer, ForeignKey('user.id'))
+
+    follower = relationship("User", foreign_keys=[
+                            follower_id], back_populates="following")
+    followed = relationship("User", foreign_keys=[
+                            followed_id], back_populates="followers")
+
+
+try:
+    result = render_er(Base, 'diagram.png')
+    print("¡Diagrama generado exitosamente! Mira el archivo diagram.png")
+except Exception as e:
+    print("Error al generar el diagrama:", e)
